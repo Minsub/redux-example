27db60f9b960ab726a7b506e3a2433f80e4db039
gray.ji <gray.ji@kakaocorp.com>
2018-07-03 14:57:36 +0900

    ADD multi counter example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App';
+import './index.css';
+
+// Redux 관련 불러오기
 import { createStore } from 'redux'
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducers);
+// 스토어 생성
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
-registerServiceWorker();
