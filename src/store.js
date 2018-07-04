b831bb1f74d3400ebc093ffb245366c57e137e26
gray.ji <gray.ji@kakaocorp.com>
2018-07-04 16:28:37 +0900

    add redux-promise-middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 
 const logger = createLogger(); // options: https://github.com/evgenyrodionov/redux-logger#options
-const store = createStore(reducers, applyMiddleware(logger, ReduxThunk));
-// const store = createStore(reducers, applyMiddleware(loggerMiddleware));
-// const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const customizedPromiseMiddleware = promiseMiddleware({
+  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
+
+const middleware = applyMiddleware(ReduxThunk, customizedPromiseMiddleware, logger);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducers, compose(middleware, devTools));
 
 export default store;
